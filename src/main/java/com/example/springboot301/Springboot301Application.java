9c6bac3ca1dcc37a20e644227f591c6a6c3b6a11
yifty08 <yifty08@icloud.com>
2019-03-12 14:50:44 -0400

    saving data using in-memory database h2

---
+++ src/main/java/com/example/springboot301/Springboot301Application.java
@@ -0,0 +1,13 @@
+package com.example.springboot301;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot301Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot301Application.class, args);
+	}
+
+}
